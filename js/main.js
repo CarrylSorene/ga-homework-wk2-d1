7591f937e91a9abd2e5d8450baeaf7456fd32177
Carryl Sorene <carrylsorene@gmail.com>
2015-08-17 21:43:10 +0100

    code nearly complete but not working, rest of sound and image files added

--- js/main.js
+++ js/main.js
@@ -1,31 +1,90 @@
-var playSound = playSound || {} //namespacing to fix clash of names used by other libraries eg setup, play, initialize - when file loads, create variable and if there's already an object with that name then use it, or create a new object for it/us to use
+var playSound = playSound || {} 
 
-//grid of scifi icons - click to play sound
-//listen for event of click to play
+// playSound.setup = function(){
+//   document.getElementById('1').addEventListener('click', playSound)
+
+window.onload = function(){
 
 soundManager.setup({
   url: '/swf/',
-  flashVersion: 9, // optional: shiny features (default = 8)
-  // optional: ignore Flash where possible, use 100% HTML5 mode
-  // preferFlash: false,
   onready: function() {
-    // Ready to use; soundManager.createSound() etc. can now be called.
-  }
-});
+    //playSound('./sounds/spaceshiphum.mp3') - not necessary to keep playing once it passed test.
+    addEventListeners()
+    }
+  });
+}
 
+function addEventListeners() {
 
+  document.getElementById('sound-1').addEventListener('click', function() {
+      playSound('spaceshiphum.mp3')
+      console.log('hello')
+  });
 
+  document.getElementById('sound-2').addEventListener('click', function() {
+      playSound('blaster.mp3')
+      console.log('hello')
+  });
 
+  document.getElementById('sound-3').addEventListener('click', function() {
+      playSound('crazytalk.mp3')
+      console.log('hello')
+  });
 
+  document.getElementById('sound-4').addEventListener('click', function() {
+      playSound('cutinout.mp3')
+      console.log('hello')
+  });
 
+  document.getElementById('sound-5').addEventListener('click', function() {
+      playSound('system_shutdown.mp3')
+      console.log('hello')
+  });
 
+  document.getElementById('sound-6').addEventListener('click', function() {
+      playSound('laserblast.mp3')
+      console.log('hello')
+  });
 
+  document.getElementById('sound-7').addEventListener('click', function() {
+      playSound('lasers.mp3')
+      console.log('hello')
+  });
 
+  document.getElementById('sound-8').addEventListener('click', function() {
+      playSound('lightbeam.mp3')
+      console.log('hello')
+  });
 
+  document.getElementById('sound-9').addEventListener('click', function() {
+      playSound('shutdown.mp3')
+      console.log('hello')
+  });
 
+  document.getElementById('sound-10').addEventListener('click', function() {
+      playSound('static.mp3')
+      console.log('hello')
+  });
 
+  document.getElementById('sound-11').addEventListener('click', function() {
+      playSound('teleport.mp3')
+      console.log('hello')
+  });
 
+  document.getElementById('sound-12').addEventListener('click', function() {
+      playSound('timemachine.mp3')
+      console.log('hello')
+  });
+}
+function playSound(sound) {
+  console.log('hi again')
+  var mySound = soundManager.createSound({
+    id: mySound,
+    url: 'sounds/' + mySound,
+    //onload: function() { 
+     // console.log('sound loaded!', this);
+  // }
+  });
+  mySound.play();
 
-
-
-//stop each previous if a new one is clicked
+}
